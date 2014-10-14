f28455f2d72157ed329c1f02dff536f9f4dbc58a
Jamie Allen <Jamieallen59@gmail.com>
2014-10-14 13:00:15 +0100

    game completed

--- src/Fizzbuzz.js
+++ src/Fizzbuzz.js
@@ -20,6 +20,7 @@ Fizzbuzz.prototype.play = function(number) {
 	if(number === 3) { return 'Fizz' }
 	else if(number === 5) { return 'Buzz' }
 	else if(number === 15) { return 'FizzBuzz' }
+	else { return number }
 };
 
 };
